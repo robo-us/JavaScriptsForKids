f71b681004108cc2fa43d0b6eb79008ce7b39a76
sp15informatyka <informatyka.sp15@gmail.com>
2016-03-11 11:56:03 +0100

    Nowe repozytorium
    Nowe repozytorium

---
+++ laczenie_tablic_z_obiektami.js
@@ -0,0 +1,7 @@
+var dinozaur = [
+  { nazwa: "Tyranozaur Rex", okres: "PóŸna Kreta" },
+  { nazwa: "Stegozaur", okres: "PóŸna Jura" },
+  { nazwa: "Plateozaur", okres: "Trias" }
+];
+dinozaur[0];
+dinozaur[0][“nazwa”]; //albo dinozaur[1].okres;
